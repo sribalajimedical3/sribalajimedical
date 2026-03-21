7ef5d23d4b378c47efb2a321857bb2560ad107f2
giftyindia <mangogaming811@gmail.com>
2026-03-21 23:25:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ app.post('/api/form', (req, res) => {
     res.json({ success: true });
 });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
     console.log("Server running on port 5000");
 });
